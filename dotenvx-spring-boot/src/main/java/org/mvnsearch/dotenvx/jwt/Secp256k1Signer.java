0ab51864eba138e97af3bcaf75f9bf93ac66293d
Libing Chen <libing.chen@gmail.com>
2025-10-03 10:17:46 +0800

    app: Secp256k1Signer

---
+++ dotenvx-spring-boot/src/main/java/org/mvnsearch/dotenvx/jwt/Secp256k1Signer.java
@@ -0,0 +1,49 @@
+package org.mvnsearch.dotenvx.jwt;
+
+import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.*;
+
+
+/**
+ * secp256k1 signer
+ */
+public class Secp256k1Signer {
+    static {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(BouncyCastleProviderSingleton.getInstance());
+        }
+    }
+
+    /**
+     * sign data with SHA256withECDSA
+     *
+     * @param data       data bytes
+     * @param privateKey private key
+     * @return signature bytes
+     */
+    public static byte[] signData(byte[] data, PrivateKey privateKey)
+            throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Signature signature = Signature.getInstance("SHA256withECDSA", "BC");
+        signature.initSign(privateKey);
+        signature.update(data);
+        return signature.sign();
+    }
+
+    /**
+     * verify signature with  with SHA256withECDSA
+     *
+     * @param data           data
+     * @param signatureBytes signature bytes
+     * @param publicKey      public key
+     * @return verified result
+     */
+    public static boolean verifySignature(byte[] data, byte[] signatureBytes, PublicKey publicKey)
+            throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Signature signature = Signature.getInstance("SHA256withECDSA", "BC");
+        signature.initVerify(publicKey);
+        signature.update(data);
+        return signature.verify(signatureBytes);
+    }
+}
